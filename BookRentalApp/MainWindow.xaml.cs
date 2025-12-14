c94b59517cc437dc252d6be14974ce1f53ba5183
Jiří Švec <st72580@upce.cz>
2025-12-14 13:42:05 +0100

    MainWindow a BookVIew XAML

--- BookRentalApp/MainWindow.xaml.cs
+++ BookRentalApp/MainWindow.xaml.cs
@@ -8,17 +8,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BookRentalApp.Views;
 
 namespace BookRentalApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    //Main okno aplikace kod pro navigaci
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            MainContent.Content = new BooksView();
+        }
+
+        private void BooksBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new BooksView();
+        }
+
+        private void CustomersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new CustomersView();
+        }
+
+        private void LoansBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new LoanView();
         }
     }
 }
--- BookRentalApp/Views/BooksView.xaml.cs
+++ BookRentalApp/Views/BooksView.xaml.cs
@@ -12,16 +12,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using BookRentalApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Windows.Controls;
+
 namespace BookRentalApp.Views
 {
-    /// <summary>
-    /// Interakční logika pro BooksView.xaml
-    /// </summary>
-    public partial class BooksView : Window
+    public partial class BooksView : UserControl
     {
         public BooksView()
         {
             InitializeComponent();
+            LoadBooks();
+        }
+
+        private void LoadBooks()
+        {
+            using var db = new AppDbContext();
+            BooksGrid.ItemsSource = db.Books.ToList();
         }
     }
 }
